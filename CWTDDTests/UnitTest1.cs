76e74e195fbadade7b97b3a939f48127b4c6c9be
NabazKamal <nabaz.kamal@gmail.com>
2020-10-21 23:48:16 +0200

    Added solution for Square

--- CWTDDTests/UnitTest1.cs
+++ CWTDDTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using NSubstitute;
 using System;
 using Xunit;
 
@@ -5,10 +6,30 @@ namespace CWTDDTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData(-1, false)]
+        [InlineData(25, true)]
+        [InlineData(2, false)]
+        [InlineData(3, false)]
+        [InlineData(4, true)]
+        [InlineData(0, true)]
+        public void Test1(double input, bool expected)
         {
+            var mock = Substitute.For<IRepository>();
+            Kata kata = new Kata();
+            bool square = kata.IsSquare(input);
+            Assert.Equal(expected, square);
+        }
 
+        private interface IRepository
+        {
+        }
+    }
+
+    public class Kata{
+        public bool IsSquare(double i)
+        {
+            return Math.Sqrt(i) % 1 == 0;
         }
     }
 }
